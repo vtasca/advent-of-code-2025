9ae95a290c77b6cfee806af605b0bf1f74d75296
vtasca <56837288+vtasca@users.noreply.github.com>
2025-12-01 15:23:56 +0000

    Add solution in progress for secret-entrance part 2

--- day-1/secret-entrance.py
+++ day-1/secret-entrance.py
@@ -10,40 +10,52 @@ def get_puzzle_input(path: Path) -> str:
 def parse_instructions(instructions: str) -> list:
     current_arrow_point = 50
     running_count = 0
+    rotation_hits = 0
 
     for instruction in instructions:
+        print(f'Current arrow point: {current_arrow_point}')
+        print(f'Current instruction: {instruction}')
+
         direction = instruction[0]
         
         count = int(instruction[1:])
         turn_count = count / 100
         count_leftover = count % 100
-        
+
         if direction == 'R':
-            if turn_count > 1:
-                current_arrow_point = current_arrow_point + count_leftover
-            elif current_arrow_point + count > 99:
-                current_arrow_point = current_arrow_point + count - 100
+            arrow_point_sum = current_arrow_point + count_leftover
+            if arrow_point_sum > 99:
+                new_arrow_point = arrow_point_sum - 100
             else:
-                current_arrow_point += count
-            
+                new_arrow_point = arrow_point_sum
 
+            if arrow_point_sum > 100:
+                rotation_hits += 1
+                print(f"Adding an overclock hit")
+        
         elif direction == 'L':
-            if turn_count > 1:
-                current_arrow_point = current_arrow_point - count_leftover
-            elif current_arrow_point - count < 0:
-                current_arrow_point = current_arrow_point - count + 100
+            arrow_point_sum = current_arrow_point - count_leftover
+            if arrow_point_sum < 0:
+                new_arrow_point = (100 + arrow_point_sum)
             else:
-                current_arrow_point -= count
+                new_arrow_point = arrow_point_sum
 
-        if current_arrow_point > 99:
-                    current_arrow_point = current_arrow_point - 100
-        elif current_arrow_point < 0:
-                    current_arrow_point = 100 - abs(current_arrow_point)
+            if arrow_point_sum < -1 and current_arrow_point != 0:
+                rotation_hits += 1
+                print(f"Adding an overclock hit")
 
-        if current_arrow_point == 0:
+        if int(turn_count):
+            rotation_hits += int(turn_count)
+            print(f'Adding {int(turn_count)} rotation hits')
+        
+        current_arrow_point = new_arrow_point
+        print(f"New arrow point: {new_arrow_point}")
+        if new_arrow_point == 0:
             running_count += 1
+
+
     
-    return running_count
+    return running_count, rotation_hits
 
     
 if __name__ == "__main__":
@@ -51,7 +63,9 @@ if __name__ == "__main__":
 
     instructions = puzzle_input.split('\n')
 
-    running_count = parse_instructions(instructions)
+    running_count, rotation_hits = parse_instructions(instructions)
     
     print(f'Count of zeroes: {running_count}')
+    print(f'Count of rotations: {rotation_hits}')
 
+    print(f'Totals: {running_count + rotation_hits}')
