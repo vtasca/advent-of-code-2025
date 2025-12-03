3002d5fa94963878f529e6d11d1e4042ecc11c45
vtasca <vlad.tasca@gmail.com>
2025-12-03 22:32:10 +0100

    Format

--- day-2/gift-shop.py
+++ day-2/gift-shop.py
@@ -14,7 +14,7 @@ def split_sequentially(s: str):
     result = []
 
     for size in range(1, n + 1):
-        chunks = [s[i:i+size] for i in range (0, n, size)]
+        chunks = [s[i : i + size] for i in range(0, n, size)]
         result.append(chunks)
 
     return result
@@ -24,61 +24,53 @@ def calculate_invalid_ids_part_1(intervals: list):
     invalid_ids = []
 
     for interval in intervals:
-
-        lower_bound = int(interval.split('-')[0])
-        upper_bound = int(interval.split('-')[-1])
+        lower_bound = int(interval.split("-")[0])
+        upper_bound = int(interval.split("-")[-1])
 
         for i in range(lower_bound, upper_bound + 1):
-
             stringified = str(i)
-            
+
             if len(stringified) % 2 == 0:
-
                 for even_number in range(2, len(stringified) + 2, 2):
-
                     halfway = int(even_number / 2)
-                    
+
                     if stringified[:halfway] == stringified[halfway:]:
                         invalid_ids.append(stringified)
 
     return invalid_ids
 
+
 def calculate_invalid_ids_part_2(intervals: list):
     invalid_ids = []
 
     for interval in intervals:
-
-        lower_bound = int(interval.split('-')[0])
-        upper_bound = int(interval.split('-')[-1])
+        lower_bound = int(interval.split("-")[0])
+        upper_bound = int(interval.split("-")[-1])
 
         for i in range(lower_bound, upper_bound + 1):
-
             stringified = str(i)
 
             invalid = False
 
             # analyze all possible sequences of the number
             for sequence in split_sequentially(stringified):
-                
                 if len(sequence) > 1:
-
                     # check if all sequences are identical
                     if len(set(sequence)) == 1:
                         invalid = True
 
-            if invalid == True:
+            if invalid:
                 invalid_ids.append(stringified)
 
     return invalid_ids
 
+
 if __name__ == "__main__":
     puzzle_input = get_puzzle_input(PUZZLE_INPUT_PATH)
 
-    intervals = puzzle_input.split(',')
+    intervals = puzzle_input.split(",")
 
     invalid_ids = calculate_invalid_ids_part_2(intervals)
 
     print(f"Invalid IDs: {invalid_ids}")
     print(f"Sum of invalid IDs: {sum([int(x) for x in invalid_ids])}")
-    
-
--- day-3/lobby.py
+++ day-3/lobby.py
@@ -8,11 +8,11 @@ def get_puzzle_input(path: Path) -> str:
         raw_input = f.read()
     return raw_input
 
+
 def calculate_two_digit_joltage(sequences: list) -> list:
     joltages = []
 
     for sequence in sequences:
-
         # find the highest number that's not on the last position
         highest_number = 0
         for index, number in enumerate(sequence):
@@ -20,30 +20,31 @@ def calculate_two_digit_joltage(sequences: list) -> list:
                 if int(number) > highest_number:
                     highest_number = int(number)
                     highest_number_index = index + 1
-                    
+
         # then find the highest number after that
         second_highest_number = 0
         for index, number in enumerate(sequence[highest_number_index:]):
             if int(number) > second_highest_number:
                 second_highest_number = int(number)
-        
+
         joltage = int(f"{highest_number}{second_highest_number}")
         joltages.append(joltage)
 
     return joltages
 
 
-def find_highest_number(sequence: str, n=0, collection=None, target=2, og_sequence=None) -> int:
-
+def find_highest_number(
+    sequence: str, n=0, collection=None, target=2, og_sequence=None
+) -> int:
     if collection is None:
         collection = {}
         og_sequence = sequence
-    
+
     highest_number = 0
     if n == target:
         print(f"Collection: {collection}")
-        ordered_numbers = sorted(collection.values(), key=lambda x: x['index'])
-        joltage = int(''.join([str(x['number']) for x in ordered_numbers]))
+        ordered_numbers = sorted(collection.values(), key=lambda x: x["index"])
+        joltage = int("".join([str(x["number"]) for x in ordered_numbers]))
         return joltage
     else:
         for index, number in enumerate(sequence):
@@ -52,31 +53,38 @@ def find_highest_number(sequence: str, n=0, collection=None, target=2, og_sequen
                 highest_number_index = index + 1
                 og_number_index = og_sequence.index(number) + 1
         n += 1
-        collection[n] = {
-            "number": highest_number,
-            "index": og_number_index
-        }
+        collection[n] = {"number": highest_number, "index": og_number_index}
 
         print(f"Found highest number: {highest_number}")
         if len(sequence[highest_number_index:]) > 0:
-            print(f"Fowardly checking the rest of the sequence: {sequence[highest_number_index:]}")
-            return find_highest_number(sequence[highest_number_index:], n, collection, target, og_sequence)
+            print(
+                f"Fowardly checking the rest of the sequence: {sequence[highest_number_index:]}"
+            )
+            return find_highest_number(
+                sequence[highest_number_index:], n, collection, target, og_sequence
+            )
         else:
-            print(f"Backwardly checking the rest of the sequence: {sequence[:highest_number_index-1]}")
-            return find_highest_number(sequence[:highest_number_index-1], n, collection, target, og_sequence)
+            print(
+                f"Backwardly checking the rest of the sequence: {sequence[: highest_number_index - 1]}"
+            )
+            return find_highest_number(
+                sequence[: highest_number_index - 1], n, collection, target, og_sequence
+            )
 
 
 def sliding_window(sequence: str, window_size: int) -> list:
-    return [sequence[i:i+window_size] for i in range(len(sequence) - window_size + 1)]
+    return [
+        sequence[i : i + window_size] for i in range(len(sequence) - window_size + 1)
+    ]
+
 
 def foo(sequence: str, target: int):
-
     print(f"Sequence: {sequence}")
     picks_left = target - 1
     pick_list = []
     pick_index = 0
     for i in range(target):
-        window = sequence[pick_index:(len(sequence)-picks_left)]
+        window = sequence[pick_index : (len(sequence) - picks_left)]
         print(f"Window: {window}")
         print(f"Making pick number: {picks_left + 1}")
         pick = max(window)
@@ -88,18 +96,19 @@ def foo(sequence: str, target: int):
         picks_left -= 1
     return pick_list
 
+
 if __name__ == "__main__":
     puzzle_input = get_puzzle_input(PUZZLE_INPUT_PATH)
 
-    sequences = puzzle_input.split('\n')
-    
-    print('-' * 100)
+    sequences = puzzle_input.split("\n")
+
+    print("-" * 100)
 
     all_joltages = []
     for sequence in sequences:
         joltage_components = foo(sequence, 12)
-        joltage = int(''.join([str(x) for x in joltage_components]))
+        joltage = int("".join([str(x) for x in joltage_components]))
         print(f"Joltage: {joltage}")
-        print('-' * 100)
+        print("-" * 100)
         all_joltages.append(joltage)
     print(f"Sum of all joltages: {sum(all_joltages)}")
