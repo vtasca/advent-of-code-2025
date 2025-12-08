912cd361c22510e8aceb56877404155b8a60a345
vtasca <vlad.tasca@gmail.com>
2025-12-08 22:24:13 +0100

    Hack together laboratories part 2 solution

--- day-8/playground.py
+++ day-8/playground.py
@@ -19,6 +19,53 @@ def create_distances_matrix(coords_list):
             distances[coords][next_coords] = distance
     return distances
 
+def build_connections(smallest, connections, max_connections=10000000000, coords_list=None):
+    connection_count = 0
+    for distance, coords in smallest.items():
+        found_first_coord = False
+        found_second_coord = False
+        first_coord_index = None
+        second_coord_index = None
+        if connection_count >= max_connections:
+            break
+        for index, conn in enumerate(connections):
+            if coords[0] in conn:
+                found_first_coord = True
+                first_coord_index = index
+            if coords[1] in conn:
+                found_second_coord = True
+                second_coord_index = index
+        
+        if found_first_coord and found_second_coord:
+            if first_coord_index != second_coord_index:
+                print(f"\n Trying to stick together {connections[first_coord_index]} and {connections[second_coord_index]}")
+                connections[first_coord_index] = connections[first_coord_index] + connections[second_coord_index]
+                connections.pop(second_coord_index)
+            connection_count += 1
+        elif found_first_coord and not found_second_coord:
+            connections[first_coord_index].append(coords[1])
+            print(f"\n Added {coords[1]} to {connections[first_coord_index]} from {coords}")
+            connection_count += 1
+        elif not found_first_coord and found_second_coord:
+            connections[second_coord_index].append(coords[0])
+            print(f"\n Added {coords[0]} to {connections[second_coord_index]} from {coords}")
+            connection_count += 1
+        elif not found_first_coord and not found_second_coord:
+            connections.append([coords[0], coords[1]])
+            print(f"\n Added {coords[0]} and {coords[1]} to {connections} from {coords}")
+            connection_count += 1
+
+        if max([len(x) for x in connections]) == len(coords_list):
+                    print(f'found it! after {connection_count} connections')
+                    # print(f'last coords: {coords}')
+                    return connections, connection_count, [coords[0], coords[1]]
+
+    sorted_connections = sorted([len(x) for x in connections], reverse=True)
+
+    print(f"\n Lengths: {sorted_connections}")
+
+    return math.prod(sorted_connections[:3]), connection_count, [coords[0], coords[1]]
+
 if __name__ == "__main__":
     puzzle_input = get_puzzle_input(PUZZLE_INPUT_PATH)
     
@@ -55,44 +102,8 @@ if __name__ == "__main__":
 
     print(f"\n Connections: {connections}")
 
-    max_connections = 1000
-    connection_count = 0
-    for distance, coords in smallest.items():
-        found_first_coord = False
-        found_second_coord = False
-        first_coord_index = None
-        second_coord_index = None
-        if connection_count >= max_connections:
-            break
-        for index, conn in enumerate(connections):
-            if coords[0] in conn:
-                found_first_coord = True
-                first_coord_index = index
-            if coords[1] in conn:
-                found_second_coord = True
-                second_coord_index = index
-        
-        if found_first_coord and found_second_coord:
-            if first_coord_index != second_coord_index:
-                print(f"Trying to stick together {connections[first_coord_index]} and {connections[second_coord_index]}")
-                connections[first_coord_index] = connections[first_coord_index] + connections[second_coord_index]
-                connections.pop(second_coord_index)
-            connection_count += 1
-        elif found_first_coord and not found_second_coord:
-            connections[first_coord_index].append(coords[1])
-            connection_count += 1
-        elif not found_first_coord and found_second_coord:
-            connections[second_coord_index].append(coords[0])
-            connection_count += 1
-        elif not found_first_coord and not found_second_coord:
-            connections.append([coords[0], coords[1]])
-            connection_count += 1
-
-    print(f"\n Connections: {connections}")
-
-    sorted_connections = sorted([len(x) for x in connections], reverse=True)
-
-    print(f"\n Lengths: {sorted_connections}")
-
-    print(f"\n Final result: {math.prod(sorted_connections[:3])}")
-
+    result, connection_count, last_coords = build_connections(smallest, connections, max_connections=1000000000, coords_list=coords_list)
+    print(f"\n Final result: {result}")
+    print(f"\n Connection count: {connection_count}")
+    print(f"\n Last coords: {last_coords}")
+    print(f"\n Last coords product: {math.prod([x[0] for x in last_coords])}")
