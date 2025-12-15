d7f386086b928d695ebd0e7f25c20182d17c709f
vtasca <56837288+vtasca@users.noreply.github.com>
2025-12-15 16:33:42 +0000

    Add fence data structure creation for day 9 part 2

--- day-9/movie-theater.py
+++ day-9/movie-theater.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from collections import defaultdict
 
-PUZZLE_INPUT_PATH = Path("day-9/puzzle-input.txt")
+PUZZLE_INPUT_PATH = Path("day-9/sample-puzzle-input.txt")
 
 
 def get_puzzle_input(path: Path) -> str:
@@ -40,11 +40,40 @@ if __name__ == '__main__':
 
     coords = [tuple(map(int, line.split(','))) for line in puzzle_input.splitlines()]
 
-    # print(coords)
+    coord_dict = coords_to_dict(puzzle_input)
+
+    print(coords)
+    print(coord_dict)
+
+    # let's first create the fence to check for belonging
+    fence = []
+    for (x1, y1), (x2, y2) in zip(coords, coords[1:]):
+        fence.append([x1, y1])
+        print(f'added checkpoint: {[x1, y1]}')
+        if x1 != x2:
+            if x1 < x2:
+                for x in range(min(x1, x2) + 1, max(x1, x2)):
+                    fence.append([x, y1])
+            elif x1 > x2:
+                for x in range(max(x1, x2) - 1, min(x1, x2), -1):
+                    fence.append([x, y1])
+
+        if y1 != y2:
+            if y1 < y2:
+                for y in range(min(y1, y2) + 1, max(y1, y2)):
+                    fence.append([x1, y])
+            elif y1 > y2:
+                for y in range(max(y1, y2) - 1, min(y1, y2), -1):
+                    fence.append([x1, y])
+    
+        print(f"Fence: {fence}")
+
+
 
     max_area = 0
     for index, coord_1 in enumerate(coords):
         for coord_2 in coords[index:]:
+
             area = calc_area(coord_1, coord_2)
             if area > max_area:
                 max_area = area
