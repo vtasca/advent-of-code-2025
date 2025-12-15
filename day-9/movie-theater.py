53bb169f51635f54f3b70b9fadefdf2db0a35604
vtasca <56837288+vtasca@users.noreply.github.com>
2025-12-15 11:53:03 +0000

    Attempt shorter solution for day 9 part 1

--- day-9/movie-theater.py
+++ day-9/movie-theater.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from collections import defaultdict
 
 PUZZLE_INPUT_PATH = Path("day-9/sample-puzzle-input.txt")
 
@@ -7,6 +8,15 @@ def get_puzzle_input(path: Path) -> str:
     with open(path, "r") as f:
         raw_input = f.read()
     return raw_input
+
+def coords_to_dict(puzzle_input):
+    points = defaultdict(list)
+
+    for line in puzzle_input.splitlines():
+        x, y = map(int, line.split(','))
+        points[x].append(y)
+
+    return dict(points)
 
 def matrixify(puzzle_input):
     coords = [tuple(map(int, line.split(','))) for line in puzzle_input.splitlines()]
@@ -20,7 +30,7 @@ def matrixify(puzzle_input):
     for coord in coords:
         matrix[coord[1]][coord[0]] = '#'
 
-    return matrix, coords
+    return matrix
 
 def calc_area(coord_1: tuple, coord_2: tuple) -> int:
     return (abs(coord_1[0] - coord_2[0]) + 1) * (abs(coord_1[1] - coord_2[1]) + 1)
@@ -28,20 +38,28 @@ def calc_area(coord_1: tuple, coord_2: tuple) -> int:
 if __name__ == '__main__':
     puzzle_input = get_puzzle_input(PUZZLE_INPUT_PATH)
 
-    matrix, coords = matrixify(puzzle_input)
+    coords = [tuple(map(int, line.split(','))) for line in puzzle_input.splitlines()]
 
-    # for row in range(len(matrix)):
-    #     for col in range(len(matrix[0])):
-    #         print(matrix[row][col], end='')
-    #     print()
+    coord_dict = coords_to_dict(puzzle_input)
 
-    max_area = 0
-    max_coords = []
-    for coord in coords:
-        for second_coord in coords:
-            rect_area = calc_area(coord, second_coord)
-            if max_area < rect_area:
-                max_area = rect_area
-                max_coords = [coord, second_coord]
+    coords_x = [int(line.split(',')[0]) for line in puzzle_input.splitlines()]
+    coords_y = [int(line.split(',')[1]) for line in puzzle_input.splitlines()]
 
-    print(f'Found largest rectangle of area {max_area} between points {max_coords[0]} and {max_coords[1]}')
+    print(sorted(coords))
+    print(coord_dict)
+
+    min_x = min(coords_x)
+    max_x = max(coords_x)
+
+    max_distance = 0
+    for min_y in coord_dict[min_x]:
+        for max_y in coord_dict[max_x]:
+            distance = abs(min_y - max_y)
+            if distance > max_distance:
+                max_distance = distance
+                corners = [[min_x, min_y], [max_x, max_y]]
+
+    area = calc_area(corners[0], corners[1])
+        
+
+    print(f'Found largest rectangle of area {area} between points {corners[0]} and {corners[1]}')
